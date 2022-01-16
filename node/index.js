05da5c5da66bd7cf812a80d210ece347834f1d31
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2022-01-16 09:51:26 +0200

    added port

--- node/index.js
+++ node/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req,res) => {
-  res.send('Hi, there again') 
+  res.send('Hi Prague, there again') 
 });
 
 app.listen(8080, () => {
